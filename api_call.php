2ae83bb0f7bda405dee8d2a038648ffd161fa7ed
Thomas Hunter <tlhunter@gmail.com>
2013-06-04 10:59:11 -0400

    Using built-in JSON formatting for PHP 5.4+. Reorganizing api_call output.

--- api_call.php
+++ api_call.php
@@ -63,7 +63,7 @@ if (strpos($response['content_type'], 'image/') === 0) {
 	echo "<body class='S{$response['http_code']}'>\n";
 	echo "<h1>$method {$response['url']}</h1>\n";
 	echo "<p><a href='index.php'>Return to Main Screen</a></p>\n";
-	echo "<h2>Response Headers</h2>\n<pre>{$response['headers_recv']}</pre>\n";
+
 	if ($body_decoded = @json_decode($body)) {
 		echo "<h2>Response Body <small>(Valid JSON)</small></h2>\n";
 		echo "<pre class='prettyprint'><code class='language-javascript'>" . json_prettify($body) . "</code></pre>\n";
@@ -71,9 +71,14 @@ if (strpos($response['content_type'], 'image/') === 0) {
 		echo "<h2>Response Body <small>(Invalid JSON)</small></h2>\n";
 		echo "<pre>$body</pre>";
 	}
+
+	echo "<h2>Response Headers</h2>\n<pre>{$response['headers_recv']}</pre>\n";
+
+	echo "<hr />\n";
 	echo "<h2>Request Headers</h2>\n<pre>{$debug['headers_sent']}</pre>\n";
 
 	if ($additional_errors) {
+		echo "<hr />\n";
 		echo "<h2>Additional OAuth Client Errors</h2>\n";
 		echo "<pre>$additional_errors</pre>\n";
 	}
@@ -81,59 +86,57 @@ if (strpos($response['content_type'], 'image/') === 0) {
 	echo "</body>\n";
 }
 
-// PHP 5.4
-//function json_prettify($json) {
-	//return json_encode(json_decode($json), JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES)
-//}
-
-// PHP 5.3
 function json_prettify($json) {
+	if (strnatcmp(phpversion(),'5.4.0') >= 0) {
+		return json_encode(json_decode($json), JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+	} else {
 
-    $result      = '';
-    $pos         = 0;
-    $strLen      = strlen($json);
-    $indentStr   = '  ';
-    $newLine     = "\n";
-    $prevChar    = '';
-    $outOfQuotes = true;
+		$result      = '';
+		$pos         = 0;
+		$strLen      = strlen($json);
+		$indentStr   = '  ';
+		$newLine     = "\n";
+		$prevChar    = '';
+		$outOfQuotes = true;
 
-    for ($i=0; $i<=$strLen; $i++) {
+		for ($i=0; $i<=$strLen; $i++) {
 
-        // Grab the next character in the string.
-        $char = substr($json, $i, 1);
+			// Grab the next character in the string.
+			$char = substr($json, $i, 1);
 
-        // Are we inside a quoted string?
-        if ($char == '"' && $prevChar != '\\') {
-            $outOfQuotes = !$outOfQuotes;
-        
-        // If this character is the end of an element, 
-        // output a new line and indent the next line.
-        } else if(($char == '}' || $char == ']') && $outOfQuotes) {
-            $result .= $newLine;
-            $pos --;
-            for ($j=0; $j<$pos; $j++) {
-                $result .= $indentStr;
-            }
-        }
-        
-        // Add the character to the result string.
-        $result .= $char;
+			// Are we inside a quoted string?
+			if ($char == '"' && $prevChar != '\\') {
+				$outOfQuotes = !$outOfQuotes;
 
-        // If the last character was the beginning of an element, 
-        // output a new line and indent the next line.
-        if (($char == ',' || $char == '{' || $char == '[') && $outOfQuotes) {
-            $result .= $newLine;
-            if ($char == '{' || $char == '[') {
-                $pos ++;
-            }
-            
-            for ($j = 0; $j < $pos; $j++) {
-                $result .= $indentStr;
-            }
-        }
-        
-        $prevChar = $char;
-    }
+			// If this character is the end of an element,
+			// output a new line and indent the next line.
+			} else if(($char == '}' || $char == ']') && $outOfQuotes) {
+				$result .= $newLine;
+				$pos --;
+				for ($j=0; $j<$pos; $j++) {
+					$result .= $indentStr;
+				}
+			}
 
-    return $result;
+			// Add the character to the result string.
+			$result .= $char;
+
+			// If the last character was the beginning of an element,
+			// output a new line and indent the next line.
+			if (($char == ',' || $char == '{' || $char == '[') && $outOfQuotes) {
+				$result .= $newLine;
+				if ($char == '{' || $char == '[') {
+					$pos ++;
+				}
+
+				for ($j = 0; $j < $pos; $j++) {
+					$result .= $indentStr;
+				}
+			}
+
+			$prevChar = $char;
+		}
+
+		return $result;
+	}
 }
