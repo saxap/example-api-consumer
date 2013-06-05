ce665dbceec1bf0e1b9b014a98cade53a367f4e9
Thomas Hunter <tlhunter@gmail.com>
2013-06-05 16:22:18 -0400

    Preventing steps from being executable if the data isn't available

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+	$config = json_decode(file_get_contents("keys.json"));
+?>
 <h1>OAuth Example Consumer</h1>
 
 <p>These scripts will allow you to test the OAuth API access within the Copy server.</p>
@@ -10,20 +13,26 @@
 			<li>Protip: Set both to blank if you want to reset all keys</li>
 		</ul>
 	</li>
-	<li><a href="get_request_token.php">Perform OAuth Dance</a>
+	<li><?php if (@$config->consumer->key && @$config->consumer->secret) { ?><a href="get_request_token.php">Perform OAuth Dance</a>
 		<ul>
 			<li>Try loggin into an existing account, creating an account, allowing, and even denying</li>
 			<li>Once it is complete, it will update the local configuration</li>
 			<li>You will want to <a href="https://www.copy.com/profile/apps" target="_blank">Revoke Apps</a> that you have allowed if you want to test the process again</li>
 		</ul>
+	<?php } else { ?>
+	<strong>You need to set a Consumer Key / Secret before you can perform the OAuth dance.</strong>
+	<?php } ?>
 	</li>
-	<li><a href="api_call.php?method=get&endpoint=user&body=">Perform API Calls</a>
+	<li><?php if (@$config->access->token && @$config->access->secret) { ?><a href="api_call.php?method=get&endpoint=user&body=">Perform API Calls</a>
 		<ul>
 			<li>Change 'method' in the URL to the type of action you want to perform (e.g. get, post, put, delete)</li>
 			<li>Change 'endpoint' in the URL to set the name of the API call (e.g. links/LINKID)</li>
 			<li>Change 'body' in the URL to be a JSON encoded body (e.g. <code>{"first_name": "Thomas"}</code>)</li>
 			<li><a href="https://www.copy.com/developer/documentation#api-calls" target="_blank">Comprehensive API Calls Listing</a></li>
 		</ul>
+	<?php } else { ?>
+	<strong>You need to perform the OAuth dance before you can make API calls.</strong>
+	<?php } ?>
 	</li>
 </ol>
 
@@ -35,7 +44,6 @@
 
 <pre>
 <?php
-	$config = json_decode(file_get_contents("keys.json"));
 
 	echo "Consumer Key:        " . @$config->consumer->key . "\n";
 	echo "Consumer Secret:     " . @$config->consumer->secret . "\n";
